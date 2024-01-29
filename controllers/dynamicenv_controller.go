ea30688def227c582b24338f179ad06806cf516f
Haim Ashkenazi <haim@babysnakes.org>
2024-01-29 10:01:10 +0200

    Small fixes (comments and invalid deps)

--- controllers/dynamicenv_controller.go
+++ controllers/dynamicenv_controller.go
@@ -59,7 +59,7 @@ type ReconcileLoopStatus struct {
 	cleanupProgress  bool
 	subsetMessages   map[string]riskifiedv1alpha1.SubsetMessages
 	consumerMessages map[string][]string
-	// Non ready consumers and subsets
+	// Non-ready consumers and subsets
 	nonReadyCS map[string]bool
 }
 
@@ -94,10 +94,6 @@ func (rls *ReconcileLoopStatus) addDeploymentMessage(subset string, tpe riskifie
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DynamicEnv object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
@@ -263,7 +259,7 @@ func (r *DynamicEnvReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	}
 
 	if rls.cleanupError != nil {
-		// While the error occurred previously these errors are less important than the main subsets loop so we apply
+		// While the error occurred previously these errors are less important than the main subsets loop, so we apply
 		// them now only if not masking.
 		rls.setErrorIfNotMasking(rls.returnError)
 	}
